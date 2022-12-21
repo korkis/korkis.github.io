bdd5e8a31a73b2ce6d2475d0445209a353004081
Isu Kim <playkorkis@gmail.com>
2022-12-21 15:08:35 +0900

    다국어 설정

---
+++ js/lang.js
@@ -0,0 +1,67 @@
+i18next
+  .use(i18nextBrowserLanguageDetector)
+  .init({
+  // lng: 'en', // default language
+  resources: {
+    en: {
+      translation: {
+        "Menu": "Menu",
+        "Service": "Service",
+        "SERVICE": "SERVICE",
+        "usefulAndFunnyApps": "Useful and Funny Apps",
+        "About": "About",
+        "enjoyLifeWithFunnyApps": "Enjoy life with funny apps!",
+        "portfolioModal1Label": "Honey Chat",
+        "portfolioModal1Description": "Find and talk to people who meets your conditions",
+        "portfolioModal1Close": "Close Window",
+        "portfolioModal2Label": "GAZUACUP",
+        "portfolioModal2Description": "You can play the traditional Korean game Panchigi online web game by charging cryptocurrency Ripple. You can create an Ripple address, deposit, and play the game with the charged money. For a healthy game, withdrawal is impossible.",
+        "portfolioModal2Close": "Close Window",
+        "portfolioModal3Label": "Always Recorder",
+        "portfolioModal3Description": "This is an Android app that allows reservation recording. Scheduled recording is usually recorded for a long time, so it is recorded in low quality to reduce capacity. However, the cost-effectiveness ratio of sound quality and capacity has been increased to maximize sound quality. Supports Korean, English and Japanese.",
+        "portfolioModal3Close": "Close Window",
+        "portfolioModal4Label": "Bit Premium",
+        "portfolioModal4Description": "This is an Android and iOS app that shows the difference in the price of the upbit cryptocurrency won and the USDT market. Compare cryptocurrency prices using today's dollars and won. The two with the most difference are marked in red and blue.",
+        "portfolioModal4Close": "Close Window",
+        "AboutTitle": "About",
+        "aboutDescription1": "ISU uses Java to create web service that are useful to users, and uses Flutter to provide funny and useful apps for Android and iOS.",
+        "aboutDescription2": "The best security is guaranteed by server configuration using Google Cloud Platform and Firebase.",
+        "Contact": "Contact",
+        "AroundTheWeb": "Around the Web",
+        "ABOUTISU": "ABOUT ISU",
+        "aboutIsuDescription": "ISU is making useful and funny apps. You can find out what's new on the {{blogLink}}",
+        "blog": "blog",
+      }
+    },
+    ko: {
+      translation: {
+        "Menu": "메뉴",
+        "Service": "서비스",
+        "SERVICE": "서비스",
+        "usefulAndFunnyApps": "재미있고 유용한 앱",
+        "About": "소개",
+        "enjoyLifeWithFunnyApps": "재미있고 유용한 앱을 시작해보세요!",
+        "portfolioModal1Label": "허니챗",
+        "portfolioModal1Description": "당신의 조건과 맞는 상대와 매칭하여 대화합니다",
+        "portfolioModal1Close": "닫기",
+        "portfolioModal2Label": "가즈아컵",
+        "portfolioModal2Description": "암호화폐 리플을 충전하여 한국전통게임 판치기 온라인 웹게임을 즐길 수 있습니다. Ripple 주소를 생성하고 입금하고 충전된 돈으로 게임을 할 수 있습니다. 건전한 게임을 위해 출금은 불가능합니다.",
+        "portfolioModal2Close": "닫기",
+        "portfolioModal3Label": "예약 녹음기",
+        "portfolioModal3Description": "예약녹음이 가능한 안드로이드 앱입니다. 예약녹음은 보통 장시간 녹화를 하기 때문에 용량을 줄이기 위해 저화질로 녹화합니다. 하지만 음질과 용량의 가성비를 높여 음질을 극대화했습니다. 한국어, 영어, 일본어를 지원합니다.",
+        "portfolioModal3Close": "닫기",
+        "portfolioModal4Label": "비트 프리미엄",
+        "portfolioModal4Description": "업비트 암호화폐 원화와 USDT 마켓의 시세 차이를 보여주는 Android 및 iOS 앱입니다. 오늘의 달러와 원화로 암호화폐 가격을 비교해보세요. 차이가 가장 큰 두 개는 빨간색과 파란색으로 표시됩니다.",
+        "portfolioModal4Close": "닫기",
+        "AboutTitle": "소개",
+        "aboutDescription1": "ISU는 Java를 사용하여 사용자에게 유용한 웹 서비스를 만들고 Flutter를 사용하여 Android 및 iOS용 재미있고 유용한 앱을 제공합니다.",
+        "aboutDescription2": "Google Cloud Platform과 Firebase를 이용한 서버 구성으로 최고의 보안을 보장합니다.",
+        "Contact": "연락처",
+        "AroundTheWeb": "웹 주변",
+        "ABOUTISU": "ISU 소개",
+        "aboutIsuDescription": "ISU는 유용하고 재미있는 앱을 만들고 있습니다. 새소식은 {{blogLink}}에서 확인하실 수 있습니다",
+        "blog": "블로그",
+      }
+    },
+  }
+});
